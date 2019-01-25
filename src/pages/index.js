7efb2cc71aa4e0022ab8fe9f97133ef512007d30
bartekus <bartekus@gmail.com>
2019-01-24 17:30:39 -0800

    Baseline

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { withSiteData } from 'react-static'
-
-export default withSiteData(() => (
-  <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to araphel.org</h1>
-  </div>
-))
